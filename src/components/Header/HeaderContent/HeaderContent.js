938fec482cd7b031b902bbcf1c43830724c457cd
Solo <dmi.sakovsky@gmail.com>
2021-12-02 10:32:18 +0300

    7

--- src/components/Header/HeaderContent/HeaderContent.js
+++ src/components/Header/HeaderContent/HeaderContent.js
@@ -8,7 +8,7 @@ export const HeaderContent = () => {
 
     useEffect(() => {
          
-        fetch(`https://jsonplaceholder.typicode.com/photos`)
+        fetch(`https://api.rawg.io/api/games?key=fc5d17fd5f594b359a91a8ec9bcd0d53`)
         .then((res) => {
             if (res.status >= 400 && res.status < 600) {
                 throw new Error('failed fething data');
@@ -16,24 +16,27 @@ export const HeaderContent = () => {
             return res.json();
         })
         .then((res) => {
+            console.log(res.results[1]);
             let mas = [];
             for (let i=0; i < 5; i++) {
-                mas.push(res[Math.round(Math.random() * res.length)]);
+                mas.push(res.results[Math.round(Math.random() * res.results.length)]);
             }
             setGotData(mas);
-            getFiveThings(res);
+            // getFiveThings(res.results);
         })
         .catch((mes) => console.log(mes));
     }, []);
 
-    const getFiveThings = (res) => {
-        setInterval(() => {
-        let mas = [];
-        for (let i=0; i < 5; i++) {
-            mas.push(res[Math.round(Math.random() * res.length)]);
-        }
-        setGotData(mas);}, 15000);
-    };
+    // const getFiveThings = (res) => {
+    //     setInterval(() => {
+    //     let mas = [];
+    //     for (let i=0; i < 5; i++) {
+    //         mas.push(res[Math.round(Math.random() * res.length)]);
+    //     }
+    //     setGotData(mas);}, 5000);
+    // };
+
+    console.log(gotData);
 
     return (
         <>
--- src/components/Header/HeaderContent/HeaderContentCard/HeaderContentCard.js
+++ src/components/Header/HeaderContent/HeaderContentCard/HeaderContentCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import cls from "./HeaderContentCard.module.css";
 
-export const HeaderContentCard = ({headerContentCardData: {title, url}}) => {
+export const HeaderContentCard = ({headerContentCardData: {background_image, genres, metacritic, name, }}) => {
     return (
         <>
             <div className={cls.card}>
                 <div className={cls.imgCard}>
-                    <img src={url} alt="Header Card"/>
+                    <img src={background_image} alt="Header Card"/>
                 </div>
-                <span className={cls.spanCard}>{title}</span>
+                <span className={cls.spanCard}>{name}</span>
             </div>
         </>
     )
