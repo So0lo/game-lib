68cf05487a560868b1a9c848169ef3403f28407c
Solo <dmi.sakovsky@gmail.com>
2021-12-19 02:33:25 +0300

    19.12.2021

--- src/components/Main/MainNav/MainNav.js
+++ src/components/Main/MainNav/MainNav.js
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import cls from "./MainNav.module.css";
 import search from "../../../img/search.png"
+import loader from "../../../img/gif/loader.gif"
 
 export const MainNav = ({changeSearchText}) => {
+    const [genres, setGenres] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     const [inputText, setInputText] = useState('');
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(`https://api.rawg.io/api/genres?key=fc5d17fd5f594b359a91a8ec9bcd0d53`)
+        .then((res) => {
+            if (res.status >= 400 && res.status < 600) {
+                throw new Error('failed fething data');
+            }
+            return res.json();
+        })
+        .then((res) => {
+            setGenres(res.results);
+        })
+        .catch((mes) => console.log(mes))
+        .finally(() => {
+            setIsLoading(false);
+        });
+    }, []);
+
 
     const changeHadler = ({target: {value}}) => {
         setInputText(() => value);
     }
+    
     return (
         <>
            <div className={cls.mainNav}>
@@ -19,7 +42,17 @@ export const MainNav = ({changeSearchText}) => {
                         value={inputText}
                         onChange={changeHadler}
                     />
-                    <div className={cls.searchWrapper} onClick={() => changeSearchText(inputText)}><img src={search} atl="search" className={cls.search}/></div>
+                    <div className={cls.searchWrapper} onClick={() => changeSearchText(inputText)}><img src={search} alt="search" className={cls.search}/></div>
+               </div>
+               <div className={cls.filterBar}>
+                   <h3 className={cls.textFilter}>Filter:</h3>
+                   <p className={cls.typeFilter}>Genres</p>
+                   {!isLoading ? genres.map((genre) => <> 
+                        <label>
+                            <input type="checkbox" className={cls.inputCheckbox}/>
+                            <div className={cls.genre}>{genre.name}</div>
+                        </label>
+                   </>) : <img src={loader} alt="loader"/> }
                </div>
            </div>
         </>
